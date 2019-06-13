67ffe4c3a7f6e5d36cf9dc07a49b23aecc3bfed1
NOMULA DHEERAJ KUMAR <nomuladheeraj@gmail.com>
2019-06-13 16:32:37 +0530

    Update split_merge_file.py

--- split_merge_file.py
+++ split_merge_file.py
@@ -34,16 +34,3 @@ with open('<PATH> /M_Merge_file') as fp:
 eng.close()
 hin.close()
 
-#Merging both of the files
-eng=open('<PATH> /E_file','r')
-hin=open('<PATH> /H_file','r')
-out=open('<PATH> /Output_merged_file','a')
-i=0
-e=eng.readline()
-h=hin.readline()
-while e and h:
-    out.write(e+' <=> '+h+'\n')
-    e=eng.readline()
-    h=hin.readline()
-eng.close()
-hin.close()
